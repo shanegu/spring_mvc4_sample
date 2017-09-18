d29e21a8c9f956fcaff03c429d2d074aaa74b62e
shanegu <5ujiedai@gmail.com>
2017-09-17 21:26:21 -0400

    initial commit

---
+++ 7-spring-mvc4-m7-validation-exercise-files/before/src/main/java/com/pluralsight/model/Attendee.java
@@ -0,0 +1,25 @@
+package com.pluralsight.model;
+
+public class Attendee {
+
+	private String name;
+	
+	private String emailAddress;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+}
